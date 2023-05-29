c0d7db02d66afef0846d37dce22f1c391dffb1f5
IRADUKUNDA Pacific Rugwizangoga <GMDAVID59@GMAIL.COM>
2023-05-29 16:53:19 +0200

    create the project

---
+++ mix.exs
@@ -0,0 +1,29 @@
+defmodule EverylogElixirClient.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :everylog_elixir_client,
+      version: "0.1.0",
+      elixir: "~> 1.14",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:httpoison, "~> 1.8"},
+      {:poison, "~> 3.1"}
+    ]
+  end
+
+end
